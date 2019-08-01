5f309a83e5cdca622e3077b5462c3eb4d6b71232
Manish Budhiraja <22340444+imbudhiraja@users.noreply.github.com>
2019-08-01 11:49:17 +0530

    normalize text across all devices

--- src/utilities/responsive-fonts.js
+++ src/utilities/responsive-fonts.js
@@ -1,8 +1,9 @@
-import { Dimensions } from 'react-native';
+import { PixelRatio, Dimensions } from 'react-native';
 
 const {
   width, height,
 } = Dimensions.get('window');
+const pixelRatio = PixelRatio.get();
 
 // Guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 350;
@@ -11,5 +12,75 @@ const guidelineBaseHeight = 680;
 const scale = (size) => (width / guidelineBaseWidth) * size;
 const verticalScale = (size) => (height / guidelineBaseHeight) * size;
 const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+const normalize = (size) => {
+  if (pixelRatio >= 2 && pixelRatio < 3) {
+    // iphone 5s and older Androids
+    if (width < 360) {
+      return size * 0.95;
+    }
 
-export { scale, verticalScale, moderateScale };
+    // iphone 5
+    if (height < 667) {
+      return size;
+    }
+
+    // iphone 6-6s
+    if (height >= 667 && height <= 735) {
+      return size * 1.15;
+    }
+
+    // older tablets
+    return size * 1.25;
+  }
+
+  if (pixelRatio >= 3 && pixelRatio < 3.5) {
+    // catch Android font scaling on small machines
+    // where pixel ratio / font scale ratio => 3:3
+    if (width <= 360) {
+      return size;
+    }
+
+    // Catch other weird android width sizings
+    if (height < 667) {
+      return size * 1.15;
+      // catch in-between size Androids and scale font up
+      // a tad but not too much
+    }
+
+    if (height >= 667 && height <= 735) {
+      return size * 1.2;
+    }
+
+    // catch larger devices
+    // ie iphone 6s plus / 7 plus / mi note
+    return size * 1.27;
+  }
+
+  if (pixelRatio >= 3.5) {
+    // catch Android font scaling on small machines
+    // where pixel ratio / font scale ratio => 3:3
+    if (width <= 360) {
+      return size;
+      // Catch other smaller android height sizings
+    }
+
+    if (height < 667) {
+      // catch in-between size Androids and scale font up
+      // a tad but not too much
+      return size * 1.2;
+    }
+
+    if (height >= 667 && height <= 735) {
+      return size * 1.25;
+    }
+
+    // catch larger phablet devices
+    return size * 1.4;
+  }
+
+  return size;
+};
+
+// export default normalize;
+
+export { scale, verticalScale, moderateScale, normalize };
